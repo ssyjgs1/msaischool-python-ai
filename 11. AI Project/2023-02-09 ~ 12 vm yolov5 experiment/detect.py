b86fc8ef1ef9c18ceb778aba079a24ea7ad8f8ce
ssyjgs1 <ssyjgs1@outlook.com>
2023-02-13 09:41:22 +0900

    update detect.py
    - detection test to image data on local storage
    - edit arguments for local environment

--- 11. AI Project/2023-02-09 ~ 12 vm yolov5 experiment/detect.py
+++ 11. AI Project/2023-02-09 ~ 12 vm yolov5 experiment/detect.py
@@ -218,9 +218,9 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'runs/train/2023-02-10-4th/weights/best.pt', help='model path or triton URL')
+    parser.add_argument('--source', type=str, default=ROOT / 'C:/Users/user/Desktop/airplane image damdang/work-no-4-yebi', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/data_birds_x.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
